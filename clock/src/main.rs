e8a9084cf084475c5acc33d9c5edf1d8a409f9ac
keketon <mas08rin10yabko@gmail.com>
2022-08-20 14:25:53 +0900

    clock v0.1.2

--- clock/src/main.rs
+++ clock/src/main.rs
@@ -1,5 +1,15 @@
-use chrono::{DateTime, Local};
+#[cfg(windows)]
+use kernel32;
+
+#[cfg(not(windows))]
+use libc;
+
+#[cfg(windows)]
+use winapi;
+
+use chrono::{DateTime, Local, TimeZone};
 use clap::{App, Arg};
+use std::mem::zeroed;
 
 struct Clock;
 
@@ -8,8 +18,61 @@ impl Clock {
         Local::now()
     }
 
-    fn set() -> ! {
-        unimplemented!()
+    #[cfg(windows)]
+    fn set<Tz: TimeZone>(t: DateTime<Tz>) -> () {
+        use chrono::{TimeZone, Weekday};
+        use winapi::{SYSTEMTIME, WORD};
+
+        let t = t.with_timezone(&Local);
+
+        let mut systime: SYSTEMTIME = unsafe { zeroed() };
+
+        let dow = match t.weekday() {
+            Weekday::Mon => 1,
+            Weekday::Tue => 2,
+            Weekday::Wed => 3,
+            Weekday::Thu => 4,
+            Weekday::Fri => 5,
+            Weekday::Sat => 6,
+            Weekday::Sun => 0,
+        };
+
+        let mut ns = t.nanosecond();
+        let mut leap = 0;
+        let is_leap_second = ns > 1_000_000_000;
+
+        if is_leap_second {
+            ns -= 1_000_000_000;
+            leap += 1;
+        }
+
+        systime.wYear = t.year() as WORD;
+        systime.wMonth = t.month() as WORD;
+        systime.wDayOfWeek = dow() as WORD;
+        systime.wHour = t.day() as WORD;
+        systime.wMinute = t.minute() as WORD;
+        systime.wSecond = (leap + t.second()) as WORD;
+        systime.wMilliseconds = (ns / 1_000_000) as WORD;
+
+        let systime_ptr = &systime as *const SYSTEMTIME;
+
+        unsafe { SetSystemTime(systime_ptr) }
+    }
+
+    #[cfg(not(windows))]
+    fn set<Tz: TimeZone>(t: DateTime<Tz>) -> () {
+        use libc::{settimeofday, suseconds_t, time_t, timeval, timezone};
+
+        let t = t.with_timezone(&Local);
+        let mut u: timeval = unsafe { zeroed() };
+
+        u.tv_sec = t.timestamp() as time_t;
+        u.tv_usec = t.timestamp_subsec_micros() as suseconds_t;
+
+        unsafe {
+            let mock_tz: *const timezone = std::ptr::null();
+            settimeofday(&u as *const timeval, mock_tz);
+        }
     }
 }
 
@@ -42,7 +105,27 @@ fn main() {
     let std = args.value_of("std").unwrap();
 
     if action == "set" {
-        unimplemented!()
+        let t_ = args.value_of("datetime").unwrap();
+
+        let parser = match std {
+            "rfc2822" => DateTime::parse_from_rfc2822,
+            "rfc3339" => DateTime::parse_from_rfc3339,
+            _ => unimplemented!(),
+        };
+
+        let err_msg = format!("Unable to parse {} according to {}", t_, std);
+        let t = parser(t_).expect(&err_msg);
+
+        Clock::set(t);
+
+        let maybe_error = std::io::Error::last_os_error();
+        let os_error_code = &maybe_error.raw_os_error();
+
+        match os_error_code {
+            Some(0) => (),
+            Some(_) => eprintln!("Unable to set the time: {:?}", maybe_error),
+            None => (),
+        }
     }
 
     let now = Clock::get();
